24bb0ef830710c8569f0c81ca66fa8343c29faad
Prapan Chulapinyo <nquantum@hotmail.com>
2017-08-13 10:52:38 +0700

    Exercise 1-9

--- hello.c
+++ hello.c
@@ -1,14 +1,25 @@
 #include <stdio.h>
-
-/* print Fahrenheit-Celsius table
-	for fahr = 0, 20, ..., 300 */
+#define IN 	1
+#define OUT 0
 
 int main()
 {
-	int fahr;
+	int c, state;
 
-	for (fahr = 0; fahr <= 300; fahr = fahr + 20)
-		printf("%3.0f\t%6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+	state = OUT;
+	while ((c = getchar()) != EOF)
+	{
+		if (c == ' ' && state == OUT)
+		{
+			state = IN;
+			printf("%c", c);
+		}
+		if (c != ' ')
+		{
+			state = OUT;
+			printf("%c", c);
+		}
+	}
 
-
+	return 0;
 }

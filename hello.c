dbd1db8eb1ee404c16e29c2aec6e017a44a79214
Prapan Chulapinyo <nquantum@hotmail.com>
2017-08-14 10:37:18 +0700

    Exercise 1-11

--- hello.c
+++ hello.c
@@ -1,22 +1,28 @@
-/* 	copy input to output, replace tab > \t,
-	backspace \b, backslash \\ */ 
-
 #include <stdio.h>
+
+#define IN 	1
+#define OUT 0
 
 int main()
 {
-	int c;
+	int c, nl, nw, nc, state;
 
+	state = OUT;
+	nl = nw = nc = 0;
 	while ((c = getchar()) != EOF)
 	{
-		if (c == '\t')
-			printf("\\t");
-		else if (c == '\b')
-			printf("\\b");
-		else if (c == '\\')
-			printf("\\\\");
-		else
-			printf("%c", c);
+		++nc;
+		if (c == '\n')
+			++nl;
+		if (c == ' ' || c == '\n' || c == '\t')
+			state = OUT;
+		else if (state == OUT)
+		{
+			state == IN;
+			++nw;
+		}
 	}
+	printf("%d %d %d\n", nl, nw, nc);
+
 	return 0;
 }

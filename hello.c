2ba48f975660870681333307728a8bf1e14ed05f
Prapan Chulapinyo <nquantum@hotmail.com>
2017-08-17 12:19:37 +0700

    fix histogram

--- hello.c
+++ hello.c
@@ -10,43 +10,47 @@
 
 int main()
 {
-	int c, i, j, nc, ntotal, overflow, state;
+	int c, i, j, nc, nmax, ncom, overflow, state;
 	int ndigit[11];
 
-	nc = 0;
-	ntotal = 0;
-	overflow = 0;
+	/* init variable for nxt function */
 	for (i = 0; i < MAX_WD; ++i)
 		ndigit[i] = 0;
+	nc = 0;
+	overflow = 0;	
 	state = OUT;
 
 	/* get no of ndigit[0-9], no of overflow */
 	while ((c = getchar()) != EOF)
 	{
 		if (c == ' ' || c == '\t' || c == '\n')
-		{
-			if (state == IN && nc > 0)
-			{
-				state = OUT;
+		{			
+			if (nc > 0)
+			{				
 				if (nc <= MAX_WD)
-				{
 					++ndigit[nc-1];
-					++ntotal;
-				}
 				else 
 					++overflow;
-				nc = 0;
-			}		
+				nc = 0;				
+			}
+			state = OUT;	
 		}	
 		else if (state == OUT)
 		{
 			state = IN;
-			++nc;
+			nc = 1;
 		}
 		else if (state == IN)
 			++nc;
 	}
-	
+
+	/* fine max no of ndigit[i] */
+	nmax = 0;
+	for (i = 0; i < MAX_WD; ++i)
+		if (ndigit[i] > nmax)
+			nmax = ndigit[i];
+	printf("max: %d\n", nmax); /* show nmax */
+
 	/* show readed word & overflow */
 	printf("%d\n", ndigit[0]);
 	printf("%d\n", ndigit[1]);
@@ -63,9 +67,14 @@ int main()
 	/* print histogram horizontal */
 	for (j = 0; j < MAX_WD; ++j)
 	{
-		printf("%2d - ", j+1);
-		for (i = 0; i < ndigit[j]*HISTOGRAM/ntotal; ++i)
-			putchar('x');	
+		printf("%2d - %3d:", j+1, ndigit[j]);
+		if (ndigit[j] > 0)
+		{
+			if ((ncom = ndigit[j]*HISTOGRAM/nmax) <= 0)
+				ncom = 1;
+			for (i = 0; i < ncom; ++i)
+				putchar('x');
+		}
 		putchar('\n');
 	}
 

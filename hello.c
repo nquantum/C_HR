3d04249f91feda176e1cc0eae513ff46cf944192
Prapan Chulapinyo <nquantum@hotmail.com>
2017-08-19 13:53:21 +0700

    mod

--- hello.c
+++ hello.c
@@ -1,7 +1,7 @@
 /* 	Charpter 1-9 Character Array */
 
 #include <stdio.h>
-#define MAXLINE 1000
+#define MAXLINE 10
 
 int getline(char line[], int maxline);
 void copy(char to[], char from[]);
@@ -15,11 +15,14 @@ int main()
 
 	max = 0;
 	while ((len = getline(line, MAXLINE)) > 0)
+	{
+		printf("%d, %s\n", len, line);
 		if (len > max)
 		{
 			max = len;
 			copy(longest, line);
 		}
+	}
 
 	if (max > 0)
 		printf("%s", longest);
@@ -29,16 +32,24 @@ int main()
 
 int getline(char s[], int lim)
 {
-	int c, i;
+	int c, i, j;
 
-	for (i=0; i<lim-1 && (c=getchar())!=EOF && c!='\n'; ++i)
-		s[i] = c;
+	j = 0;
+	for (i=0; (c=getchar())!=EOF && c!='\n'; ++i)
+	{
+		if (i < lim-2) /* 1000 - 2 = 998 (0-997) */
+		{
+			s[j] = c;
+			++j;
+		}
+	}
 	if (c == '\n')
 	{
-		s[i] = '\n';
+		s[j] = '\n';
+		++j;
 		++i;
 	}
-	s[i] = '\0';
+	s[j] = '\0';
 	return i;
 }
 

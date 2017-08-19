ad506fed3752a72e2410a6b8d44fbc19e90100be
nQuantum <nquantum.pop@gmail.com>
2017-08-19 09:43:00 +0700

    Charpter 1.9 Character Arrays

--- hello.c
+++ hello.c
@@ -1,115 +1,52 @@
-/* 	Exercise 1-14 print histogram of 
-	the frequency of difference of words
-	horizontal, vertical */
+/* 	Charpter 1-9 Character Array */
 
 #include <stdio.h>
-#include <ctype.h>
+#define MAXLINE 1000
 
-#define IN 			0
-#define OUT 		1
-#define MAX_WD		128
-#define HISTOGRAM 	40
+int getline(char line[], int maxline);
+void copy(char to[], char from[]);
 
 int main()
 {
-	int c, i, j, nc, nmax, overflow, state;
-	int ncom;
-/*	int ncom[MAX_WD];	*/
-	int ndigit[MAX_WD];
+	int len;
+	int max;
+	char line[MAXLINE];
+	char longest[MAXLINE];
 
-	/* init variable for nxt function */
-	for (i = 0; i < MAX_WD; ++i)
-		ndigit[i] = 0;
-	nc = 0;
-	overflow = 0;	
-	state = OUT;
-
-	/* get no of ndigit[0-9], no of overflow */
-	while ((c = getchar()) != EOF)
-	{
-/*		if (c == ' ' || c == '\t' || c == '\n')
-		{			
-			if (nc > 0)
-			{				
-				if (nc <= MAX_WD)
-					++ndigit[nc-1];
-				else 
-					++overflow;
-				nc = 0;				
-			}
-			state = OUT;	
-		}	
-		else if (state == OUT)
+	max = 0;
+	while ((len = getline(line, MAXLINE)) > 0)
+		if (len > max)
 		{
-			state = IN;
-			nc = 1;
+			max = len;
+			copy(longest, line);
 		}
-		else if (state == IN)
-			++nc;	*/
-		if (c < MAX_WD)
-			++ndigit[c];
-		else
-			++overflow;
-	}
 
-	/* find max no of ndigit[i] */
-	nmax = 0;
-	for (i = 0; i < MAX_WD; ++i)
-		if (ndigit[i] > nmax)
-			nmax = ndigit[i];
-	printf("max: %d\n", nmax); /* show nmax */
-
-	printf("overflow: %d\n", overflow);
-
-	/* show readed word & overflow */
-/*	for (i = 0; i < MAX_WD; ++i)
-		printf("%c: %d\n",'a'+i, ndigit[i]);
-	printf("overflow: %d\n", overflow);	*/
-
-	/* print histogram horizontal */
-	for (j = 0; j < MAX_WD; ++j)
-	{
-		if (isprint(j))
-			printf("%2c - ", j);
-		else 
-			printf("   - ");
-		printf("%3d:", ndigit[j]);
-		if (ndigit[j] > 0)
-		{
-			if ((ncom = ndigit[j]*HISTOGRAM/nmax) <= 0)
-				ncom = 1;
-			for (i = 0; i < ncom; ++i)
-				putchar('x');
-		}
-		putchar('\n');
-	}
-
-	/* print vertical */
-	/* convert no to histogram */
-/*	for (i = 0; i < MAX_WD; ++i)
-		if (ndigit[i] > 0)
-		{
-			if ((ncom[i] = ndigit[i]*HISTOGRAM/nmax) <= 0)
-				ncom[i] = 1;
-		}
-		else
-			ncom[i] = 0;	*/
-	/* write histogram */
-/*	for (i = HISTOGRAM; i > 0; --i)
-	{
-		for (j = 0; j < MAX_WD; ++j)
-		{
-			if (i <= ncom[j])
-				printf(" x ");
-			else
-				printf("   ");			
-		}
-		putchar('\n'); 
-	}	*/
-	/* write topic */
-/*	for (i = 0; i < MAX_WD; ++i)
-		printf(" %1d ", i + 1);
-	putchar('\n');	*/
+	if (max > 0)
+		printf("%s", longest);
 
 	return 0;
+}
+
+int getline(char s[], int lim)
+{
+	int c, i;
+
+	for (i=0; i<lim-1 && (c=getchar())!=EOF && c!='\n'; ++i)
+		s[i] = c;
+	if (c == '\n')
+	{
+		s[i] = '\n';
+		++i;
+	}
+	s[i] = '\0';
+	return i;
+}
+
+void copy(char to[], char from[])
+{
+	int i;
+
+	i = 0;
+	while ((to[i] = from[i]) != '\0')
+		++i;
 }

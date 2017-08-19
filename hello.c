615275981e109927855e25ecb3fbcd5cc8483611
Prapan Chulapinyo <nquantum@hotmail.com>
2017-08-19 20:52:40 +0700

    Exercise 1-17 finish

--- hello.c
+++ hello.c
@@ -1,31 +1,23 @@
-/* 	Charpter 1-9 Character Array */
+/* 	Exercise 1-17 print all input lines that
+	longer than 80 characters */
 
 #include <stdio.h>
-#define MAXLINE 10
+#define MAXLINE 1000
+#define LIMIT 	5
 
-int getline(char line[], int maxline);
-void copy(char to[], char from[]);
+int getline(char line[], int m);
 
 int main()
 {
 	int len;
-	int max;
 	char line[MAXLINE];
-	char longest[MAXLINE];
 
-	max = 0;
 	while ((len = getline(line, MAXLINE)) > 0)
 	{
-		printf("%d, %s\n", len, line);
-		if (len > max)
-		{
-			max = len;
-			copy(longest, line);
-		}
+/*		printf("%d, %s", len, line); */
+		if (len > LIMIT)
+			printf("%s", line);
 	}
-
-	if (max > 0)
-		printf("%s", longest);
 
 	return 0;
 }
@@ -34,14 +26,10 @@ int getline(char s[], int lim)
 {
 	int c, i, j;
 
-	for (i=0, j=0; (c=getchar())!=EOF && c!='\n'; ++i)
-	{
-		if (i < lim-2) /* 1000 - 2 = 998 (0-997) */
-		{
+	for (i=0, j=0; (c=getchar())!=EOF && c!='\n'; ++i, j++)
+		if (j < lim - 2)	
 			s[j] = c;
-			++j;
-		}
-	}
+
 	if (c == '\n')
 	{
 		s[j] = '\n';
@@ -49,14 +37,6 @@ int getline(char s[], int lim)
 		++i;
 	}
 	s[j] = '\0';
+
 	return i;
-}
-
-void copy(char to[], char from[])
-{
-	int i;
-
-	i = 0;
-	while ((to[i] = from[i]) != '\0')
-		++i;
 }

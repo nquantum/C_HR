b24d4bc46941d8d0b2cf3007668a8b8a27f60c58
Prapan Chulapinyo <nquantum@hotmail.com>
2017-08-22 10:16:48 +0700

    Exercise 1-20 detab

--- hello.c
+++ hello.c
@@ -1,24 +1,22 @@
-/* 	Exercise 1-19 function reverse */
+/* 	Exercise 1-20 function detab */
 
 #include <stdio.h>
 #define MAXLINE 1000
-#define LIMIT 	5
+#define TAB 	4
 
 int getline(char line[], int m);
-void reverse(char to[], char from[]);
+void detab(char to[], char from[]);
 
 int main()
 {
-	int i, len;
 	char line[MAXLINE];
 	char store[MAXLINE];
 
 	while (getline(line, MAXLINE) > 0)
-	{	
-		printf("%s", line);
-		reverse(store, line);
+	{
+		detab(store, line);
 		printf("%s", store);
-	}	
+	}
 
 	return 0;
 }
@@ -27,10 +25,12 @@ int getline(char s[], int lim)
 {
 	int c, i, j;
 
-	for (i=0, j=0; (c=getchar())!=EOF && c!='\n'; ++i, j++)
-		if (j < lim - 2)	
+	for (i=0, j=0; (c=getchar())!=EOF && c!='\n'; ++i)
+		if (j < lim - 2)
+		{	
 			s[j] = c;
-
+			++j;
+		}
 	if (c == '\n')
 	{
 		s[j] = '\n';
@@ -42,20 +42,30 @@ int getline(char s[], int lim)
 	return i;
 }
 
-void reverse(char to[], char from[])
+void detab(char to[], char from[])
 {
-	int i, j;
+	int c, i, j, k;
 
-	for (i = 0; from[i] != '\n'; ++i)
-		;
-	--i;
-
-	j = 0;
-	while (i > 0)
+	i = j = k = 0;
+	while ((c = from[i]) != '\0')
 	{
-		to[j] = from[i];
-		++j;
-		--i;
+		if (c == '\t')
+		{
+			while (k <= j)
+				k = k + TAB + 1;
+			while (j < k)
+			{
+				to[j] = ' ';
+				++j;
+			}
+			i++;
+		}
+		else
+		{
+			to[j] = c;
+			i++;
+			j++;
+		}		
 	}
-	to[j] = from[i];
+	to[j] = '\0';
 }

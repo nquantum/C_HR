1f59f1ada8c2606d19df5b2d97e7939421a51063
Prapan Chulapinyo <nquantum@hotmail.com>
2017-08-24 10:06:01 +0700

    Exercise 1-21 entab - by me

--- hello.c
+++ hello.c
@@ -1,21 +1,65 @@
-/* 	Exercise 1-20 function detab */
+/* 	Exercise 1-21 function entab */
 
 #include <stdio.h>
 #define MAXLINE 1000
-#define TAB 	4
+#define TAB 	8
 
 int getline(char line[], int m);
 void detab(char to[], char from[]);
 
 int main()
 {
+	int c, i, j, k, space, tab;
 	char line[MAXLINE];
 	char store[MAXLINE];
 
-	while (getline(line, MAXLINE) > 0)
+
+	while ((c = getline(line, MAXLINE)) > 0)
 	{
-		detab(store, line);
-		printf("%s", store);
+/*		printf("%s<org\n", line); */
+
+		i = j = space = tab = 0;
+		while ((c = line[i]) != '\0')
+		{
+			if (c == ' ')
+			{				
+				while (line[i] == ' ')
+				{
+					if ((i % TAB == 0) && (space > 0))
+					{
+						++tab;
+						space = 0;
+					}
+					else
+					{
+						++space;
+					}
+					++i;
+				}
+
+				if (i % TAB == 0)
+				{
+					++tab;
+					space = 0;
+				}			
+				else if (tab > 0)
+					++space;
+
+				for (k = 0; k < tab; ++k, ++j)
+					store[j] = '\t';
+				for (k = 0; k < space; ++k, ++j)
+					store[j] = ' ';
+			}
+			else
+			{
+				store[j] = c;
+				++i;
+				++j;
+			}
+		}
+		store[j] = '\0';
+
+/*		printf("%s<mod\n", store); */
 	}
 
 	return 0;

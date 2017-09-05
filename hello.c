b485b7764f459b2507416000a58bf7c76536a83d
Prapan Chulapinyo <nquantum@hotmail.com>
2017-09-05 15:09:40 +0700

    progressing

--- hello.c
+++ hello.c
@@ -2,22 +2,57 @@
 
 #include <stdio.h>
 
-int getline();
+#define MAX			1000
+#define N 			20
+#define IN_WORD		1
+#define END_WORD	0
+
+int getline(char input[], int limit);
 
 int main()
 {
+	int c, i, j, state;
+	char line[MAX];
 
-	while (getline() != 0;)
+	state = END_WORD;
+	pos = 0;
+	while (getline(line, MAX) != 0)
 	{
-		while -not end sentence-
-			read space , pos space_no
-			read word , pos word_no
-			if end word | end column
-				endword> show space, word
-			else
-				end column> 
-				if 0space show word, \n at n-1
-				if >0space \n, 0space, move pos begin of word
+		while ((c = line[pos]) != '\0')
+		{
+			if (c != ' ')
+			{	
+				if (pos == 'N')//end word | end column
+					for (; i > 0; --i) //endword> show space, word
+					{
+						--pos;
+						putchar('\n');
+					}
+				else if (state == END_WORD)
+				{
+					state = IN_WORD;
+					i = 1;;
+				}
+				else if (state == IN_WORD)
+					++i;
+			}
+			else if (c == ' ')
+			{
+				if (state == IN_WORD)
+				{
+					for (j = i; j > 0; --j)
+						--pos;
+				
+					for (; i > 0; --i)
+						putchar(line[pos]);
+				}
+				if (pos == 'N')
+					putchar('\n');
+				putchar(' ');
+			}
+			++pos;
+		}				
 	}
+
 	return 0;
 }

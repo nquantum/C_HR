60a117f6cd2212a863cb876ca2acb2f9b2d983a8
Prapan <nquantum@hotmail.com>
2017-09-18 00:49:34 +0700

    pass 1

--- hello.c
+++ hello.c
@@ -3,56 +3,67 @@
 #include <stdio.h>
 
 #define MAX			1000
-#define N 			20
-#define IN_WORD		1
-#define END_WORD	0
+#define N 			19
 
 int getline(char input[], int limit);
+void cutline(char input[]);
 
 int main()
 {
-	int c, i, j, state;
 	char line[MAX];
 
-	state = END_WORD;
-	pos = 0;
 	while (getline(line, MAX) != 0)
-	{
-		while ((c = line[pos]) != '\0')
-		{
-			if (c != ' ')
-			{	
-				if (pos == 'N')//end word | end column
-					for (; i > 0; --i) //endword> show space, word
-					{
-						--pos;
-						putchar('\n');
-					}
-				else if (state == END_WORD)
-				{
-					state = IN_WORD;
-					i = 1;;
-				}
-				else if (state == IN_WORD)
-					++i;
-			}
-			else if (c == ' ')
-			{
-				if (state == IN_WORD)
-				{
-					for (j = i; j > 0; --j)
-						--pos;
-				
-					for (; i > 0; --i)
-						putchar(line[pos]);
-				}
-				if (pos == 'N')
-					putchar('\n');
-				putchar(' ');
-			}
-			++pos;
-		}				
+	{		
+		cutline(line);					
 	}
 
 	return 0;
+}
+
+int getline(char in[], int lim)
+{
+	int c, i;
+
+	i = 0;
+	while ((c = getchar()) != EOF)
+	{
+		if (i < lim)
+		{		
+			in[i] = c;
+			++i;
+		}
+	}
+	in[i] = '\0';
+
+/*	printf("%s", in);	*/
+
+	return i;
+}
+
+void cutline(char in[])
+{
+	int i;
+	int pos;  /* pos input */
+	int no; /* number of character */
+
+  	pos = no = 0;
+  	while (in[pos] != '\0')
+  	{
+    	if (no < N)
+    	{
+      		++no;
+      		++pos;
+    	}
+    	else
+    	{
+     		for (i = no; i > 0; --i)
+          		putchar(in[pos-i]);
+      		putchar('\n');
+      		no = 0;
+    	}
+  	}
+  	for (i = no; i > 0; --i)
+      putchar(in[pos-i]);
+
+  	return;
 }

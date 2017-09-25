f3f66d6298ea946e6dff2dd4ee986282b86030b8
Prapan <nquantum@hotmail.com>
2017-09-25 10:56:48 +0700

    exercise 2-3 write htoi(s)

--- hello.c
+++ hello.c
@@ -1,33 +1,11 @@
-/* 	Exercise 2-2 write loop program instead for */
+/* 	Exercise 2-3 write htoi(s) , x and X */
 
 #include <stdio.h>
-
-#define LIM 100
-
-enum loop { NO, YES };
-enum loop okloop = NO;
+#include <ctype.h>
 
 int main()
 {
-	int i = 0;
-	int c;
-	char s[LIM];
-
-	while (i < LIM - 1)
-	{
-		if ((c = getchar()) != '\n')
-			if (c != EOF)
-				s[i] = c;
-			else
-				break;
-		else
-			break;
-		++i;
-	}
-	s[i] = '\0';
-
-	printf("%s\n", s);
-	printf("%d\n", okloop);
+	
 	
 	return 0;
 }

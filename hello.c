78098f50edc82d7ef1d8ca07d77eb0b19d00e85a
Prapan <nquantum@hotmail.com>
2017-09-26 22:24:30 +0700

    mod some:

--- hello.c
+++ hello.c
@@ -1,24 +1,23 @@
 /* 	Exercise 2-3 write htoi(s) , x and X */
 
 #include <stdio.h>
-#include <ctype.h>
 
 #define MAX	1000
 
-int htoi(char in[]);
+int atoi(char in[]);
 
 int main()
 {
 	char store1[MAX] = "7654321";	
 	char store2[MAX] = "54321";
 
-	printf("store1 = %s > %d\n", store1, htoi(store1));
-	printf("store2 = %s > %d\n", store2, htoi(store2));
+	printf("store1 = %s > %d\n", store1, atoi(store1));
+	printf("store2 = %s > %d\n", store2, atoi(store2));
 	
 	return 0;
 }
 
-int htoi(char store[])
+int atoi(char store[])
 {
 	int i, out;
 

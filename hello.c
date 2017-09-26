f1f9b95e1be99b80ebd7cbce831fc6008f887c3d
Prapan <nquantum@hotmail.com>
2017-09-26 11:09:31 +0700

    now can convert

--- hello.c
+++ hello.c
@@ -9,11 +9,11 @@ int htoi(char in[]);
 
 int main()
 {
-	char store1[MAX] = "20";	
-	char store2[MAX] = "123";
+	char store1[MAX] = "7654321";	
+	char store2[MAX] = "54321";
 
-	printf("store1 = %d\n", htoi(store1));
-	printf("store2 = %d\n", htoi(store2));
+	printf("store1 = %s > %d\n", store1, htoi(store1));
+	printf("store2 = %s > %d\n", store2, htoi(store2));
 	
 	return 0;
 }
@@ -22,9 +22,10 @@ int htoi(char store[])
 {
 	int i, out;
 
-	for (i = 0; store[i] != '\0'; ++i)
+	for (i = 0, out = 0; store[i] != '\0'; ++i)
 	{
-		printf("i = %d, store[i] = %d\n", i, store[i] - '0');
+		out = out * 10  + store[i] - '0';
+/*		printf("%d\n", out); */
 	}
-	return 0;
+	return out;
 }

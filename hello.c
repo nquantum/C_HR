ed81ba2c1538f3bedd7b13cd87ee9c4a0a2e420b
Prapan <nquantum@hotmail.com>
2017-09-26 23:48:56 +0700

    finish htoi

--- hello.c
+++ hello.c
@@ -4,6 +4,8 @@
 
 #define MAX	1000
 
+enum test { INVALID, VALID };
+
 int atoi(char in[]);
 int htoi(char in[]);
 
@@ -11,7 +13,7 @@ int main()
 {
 	char store1[MAX] = "7654321";	
 	char store2[MAX] = "54321";
-	char store3[MAX] = "0x123cdef";
+	char store3[MAX] = "0x123";
 
 	printf("store1 = %s > %d\n", store1, atoi(store1));
 	printf("store2 = %s > %d\n", store2, atoi(store2));
@@ -34,15 +36,34 @@ int atoi(char store[])
 
 int htoi(char store[])
 {
-	int i, out;
+	int i, out, state;
 
-	for (i = 2, out = 0; store[i] != '\0'; ++i)
-	{
-		if (store[i] >= '0' && store[i] <= '9')
-			out = out * 16 + store[i] - '0';
-		else
-			out = out * 16 + store[i] - 'a' + 10;
-	}
+	if (store[0] == '0' && (store[1] == 'x' || store[1] != 'X'))
+		for (i = 2; store[i] != '\0'; ++i)
+		{
+			if ((store[i] >= '0' && store[i] <= '9') || (store[i] >= 'a' && store[i] <= 'f') || (store[i] >= 'A' && store[i] <= 'F'))
+				state = VALID;
+			else
+			{
+				state = INVALID;
+				break;
+			}
+		}
+	else
+		state = INVALID;
+
+	if (state == VALID)
+		for (i = 2, out = 0; store[i] != '\0'; ++i)
+		{
+			if (store[i] >= '0' && store[i] <= '9')
+				out = out * 16 + store[i] - '0';
+			else if (store[i] >= 'a' && store[i] <= 'f')
+				out = out * 16 + store[i] - 'a' + 10;
+			else if (store[i] >= 'A' && store[i] <= 'F')
+				out = out * 16 + store[i] - 'A' + 10; 
+		}
+	else 
+		out = 0;
 
 	return out;
 }

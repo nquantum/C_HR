17b2965c90b6f4db9028db3941b3a64b61b87b83
Prapan <nquantum@hotmail.com>
2017-09-28 12:03:20 +0700

    exercise 2.5 any)s1, s2)

--- hello.c
+++ hello.c
@@ -1,21 +1,33 @@
-/* 	Exercise 2-4 write squeeze(s1, s2) , del s1 frm each char s2 */
+/* 	Exercise 2-5 write any(s1, s2) , return 1st location occurs of s2 */
 
 #include <stdio.h>
 
 #define	MAX	100
 
+int any(char s1[], char s2[]);
+
 int main()
 {
-	int i, j;
-	int n = 'l';
-	char s[MAX] = "Hello test\n";
-
-	for (i = j = 0; s[j] != '\0'; ++j)
-		if (s[j] != n)
-			s[i++] = s[j];	
-	s[i] = '\0';
+	char s[MAX] = "abcdefghijklmnopqrstuvwxyz\n";
+	char test[MAX] = "1";
 
 	printf("%s", s);
+	printf("%s\n", test);
+	printf("%d\n", any(s, test));
 
 	return 0;
+}
+
+int any(char in[], char comp[])
+{
+	int i, pos;
+
+	for (pos = 0; in[pos] != '\0'; ++pos)
+	{
+		for (i = 0; comp[i] != '\0'; ++i)
+			if (in[pos] == comp[i])
+				return pos;
+	}
+
+	return -1;
 }

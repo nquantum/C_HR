1fdde4e1e0783ea0e1a0761b5c6408af94212bfa
Prapan <nquantum@hotmail.com>
2017-09-29 20:51:48 +0700

    finish 2-6

--- hello.c
+++ hello.c
@@ -7,17 +7,18 @@ int setbits(int x, int p, int n, int y);
 int main()
 {
 	int a = 0xFF;
-	printf("%d\n%d\n", a, setbits(a, 0xFF, 0, 0));
+	int b = 0xF9;
+
+	printf("%d\n%d\n", a, setbits(a, 2, 4, b));
 
 	return 0;
 }
 
 int setbits(int x, int p, int n, int y)
 {
-	int clrbits = 0xF0;
-	int setbits = 0;
+/*	int y_mod = y & ~(~0 << n);
+	int x_mod = x & ((~0 << p + n) | ~(~0 << p));
 
-	x = x & clrbits;
-
-	return x;
+	return x_mod | y_mod << p;*/
+	return (x & ((~0 << p + n) | ~(~0 << p))) | (y & ~(~0 << n)) << p;
 }

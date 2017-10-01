21e3ce989e539d136b8c9583b4e68ed3e21c61fe
Prapan <nquantum@hotmail.com>
2017-10-01 09:02:17 +0700

    exercise 2-9 lower

--- hello.c
+++ hello.c
@@ -1,37 +1,20 @@
-/* 	Exercise 2-8 write rightrot(x, n) rotate to the right of n bit  */
+/* 	Exercise 2-9 write int lower(int c) using conditional expression */
 
 #include <stdio.h>
-#include <limits.h>
 
-unsigned int rightrot(unsigned int x, int n);
-int find_l(unsigned int x);
+int lower(int c);
 
 int main()
 {
-	unsigned int x = 10;
-	int n = 4;
+	int c;
 
-/*	printf("uint_max %u\n", UINT_MAX);
-	printf("rotate time: %d\n", find_l(x));	*/
-	printf("%u\n%u\n", x, rightrot(x, n));
+	while ((c=getchar()) != EOF)
+		printf("%c", lower(c));
 
 	return 0;
 }
 
-unsigned int rightrot(unsigned int x, int n)
-{		
-	unsigned int x_tmp = x >> n;
-	int l = find_l(x);
-	unsigned int r_tmp = (x & ~(~0 << n)) << l - n;
-	return x_tmp | r_tmp;
-}
-
-int find_l(unsigned int x)
-{
-	int i =0;
-
-	for (x = x | ~0; x != 0; x = x >> 1)
-		++i;
-
-	return i;
+int lower(int in)
+{	
+	return (in >= 'A' && in <= 'Z') ? in + 'a' - 'A' : in;
 }
